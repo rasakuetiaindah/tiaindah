ddf61b1905146f46af73a678f742e3241221e5cc
rasakue.tiaindah <rasakue.tiaindah@gmail.com>
2024-11-25 08:16:34 +0000

    updatetoml end add file riderect

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).json({
-    message:'halaman home'
+    message:'halaman home '
   })
 });
 
